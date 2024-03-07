90bad296c4759006551fae1a8f702dc0e2cc7bbd
baijianjiang <baijianjiang@wilcom.com.cn>
2024-03-07 18:34:29 +0800

    SpringSecurity快速入门

---
+++ Lab01-SpringSecurity/src/main/java/com/skywalker/springsecurity/Application.java
@@ -0,0 +1,15 @@
+package com.skywalker.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author baijj
+ * @Date 2024/03/05 15:45
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
